da7869548e2ce0ee52e123839aa93c72811b92ba
Clement Pinet <pinetclement@gmail.com>
2013-12-29 10:12:17 +0100

    glBegin glEnd optimization in lightmanager.

--- src/light/ShadowBuffer.java
+++ src/light/ShadowBuffer.java
@@ -2,7 +2,7 @@ package light;
 
 public class ShadowBuffer {
 	public int lastShadow = 0;
-	private static int size = 10000;
+	private static int size = 2000;
 	private Shadow[] shadows = new Shadow[size];
 	public ShadowBuffer() {
 		for(int i = 0 ; i < size ; i++){
