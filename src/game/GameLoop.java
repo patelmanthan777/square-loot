346a69fac22df2151d1b0da7a44afab51ed40b5a
Clement Pinet <pinetclement@gmail.com>
2014-01-10 18:10:03 +0100

    Merge of buffers

--- src/game/GameLoop.java
+++ src/game/GameLoop.java
@@ -80,7 +80,7 @@ public class GameLoop {
 		controle = new Control(p);
 		BlockFactory.initBlocks();
 
-		map = new Map(new Vector2f(4,4), new Vector2f(12,10), new Vector2f(48,48));
+		map = new Map(new Vector2f(10,10), new Vector2f(12,10), new Vector2f(48,48));
 		map.renderMapToFrameBuffers();
 		p.setPosition(map.getSpawnPixelPosition());
 		ProjectileManager.init();
