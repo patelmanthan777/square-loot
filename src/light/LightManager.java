f026ea599cafe57bed5e4f193e7848f3bfd0932a
Clement Pinet <pinetclement@gmail.com>
2014-01-25 15:59:39 +0100

    void block layer is not drawn in the light manager

--- src/light/LightManager.java
+++ src/light/LightManager.java
@@ -81,8 +81,8 @@ public class LightManager {
 		Light l = new PointLight(p, color, radius, maxDst, dynamic);
 		l.setName(name);
 
-		ShadowBuffer[] shadows = new ShadowBuffer[Map.maxLayer];
-		for (int i = 0; i < Map.maxLayer; i++) {
+		ShadowBuffer[] shadows = new ShadowBuffer[Map.maxLayer-1];
+		for (int i = 0; i < Map.maxLayer-1; i++) {
 			shadows[i] = new ShadowBuffer();
 		}
 
@@ -140,8 +140,8 @@ public class LightManager {
 			Vector2f dir) {
 		Laser laser = new Laser(p, color, dir);
 
-		ShadowBuffer[] shadows = new ShadowBuffer[Map.maxLayer];
-		for (int i = 0; i < Map.maxLayer; i++) {
+		ShadowBuffer[] shadows = new ShadowBuffer[Map.maxLayer-1];
+		for (int i = 0; i < Map.maxLayer-1; i++) {
 			shadows[i] = new ShadowBuffer();
 		}
 
@@ -167,7 +167,7 @@ public class LightManager {
 	static public void updateLightShadows(Light l) {
 		/* Set to 0 the pointer to the last shadow */
 		ShadowBuffer[] shadows = lightShadows.get(l);
-		for (int i = 0; i < Map.maxLayer; i++) {
+		for (int i = 0; i < Map.maxLayer-1; i++) {
 			shadows[i].lastShadow = 0;
 		}
 		for (ShadowCaster sc : shadowCasters) {
@@ -298,7 +298,7 @@ public class LightManager {
 							-(Map.currentBufferPosition.y + j * Map.textureSize),
 							0);
 
-					for (int layer = 0; layer < Map.maxLayer; layer++) {
+					for (int layer = 0; layer < Map.maxLayer-1; layer++) {
 						ambiantShader.use();
 						ambiantShader.setUniform3f("color", 1.0f, 1.0f, 1.0f);
 						ambiantShader.setUniform1f("power", 0.05f);
